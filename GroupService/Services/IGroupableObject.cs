4d30cc96c04c6cff1a95d4365db203157c4b9e92
Robin Gimenez <gimenez.robin11@gmail.com>
2021-11-25 15:35:36 +0100

    push groups

---
+++ GroupService/Services/IGroupableObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace GroupService.Services
+{
+    [ServiceContract]
+    public interface IGroupableObject
+    {
+        public string Name { get; set; }
+        public int GroupId { get; set; }
+        public List<int> Members { get; set; }
+
+        [OperationContract]
+        public Group GetGroup(int id);
+    }
+}
